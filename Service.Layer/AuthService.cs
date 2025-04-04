643542edf4284daa6f49249a902352b7ac016bff
shehabwaleed2022 <shehabwaleed2010@gmail.com>
2025-04-04 11:28:43 +0200

    feat: Change the claims names

--- Service.Layer/AuthService.cs
+++ Service.Layer/AuthService.cs
@@ -39,10 +39,10 @@ namespace Service.Layer
         {
             var authClaims = new List<Claim>
             {
-                new Claim(ClaimTypes.Email, user.Email),
+                new Claim("Email", user.Email),
                 new Claim(ClaimTypes.NameIdentifier, user.Id),
-                new Claim(ClaimTypes.SerialNumber , user.NationalId),
-                new Claim(ClaimTypes.GivenName , user.DisplayName),
+                new Claim("NationalId" , user.NationalId),
+                new Claim("Name" , user.DisplayName),
                 new Claim("ID" , user.Id),
             };
 
@@ -50,7 +50,7 @@ namespace Service.Layer
 
             foreach (var role in roles)
             {
-                authClaims.Add(new Claim(ClaimTypes.Role, role));
+                authClaims.Add(new Claim("Role", role));
             }
             
             var authKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:SecurityKey"]));
@@ -175,7 +175,7 @@ namespace Service.Layer
             var jwtToken = handler.ReadJwtToken(token);
 
             
-            var emailClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email || c.Type == "email").Value;
+            var emailClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == "Email" || c.Type == "email").Value;
 
             var user = await _userManager.FindByEmailAsync(emailClaim);
 
